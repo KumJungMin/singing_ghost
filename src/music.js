39e506e54b38d6be3d0791044e3dd646f5cceb43
KumJungMin <37934668+KumJungMin@users.noreply.github.com>
2021-05-16 13:14:07 +0900

    Update music.js

--- src/music.js
+++ src/music.js
@@ -4,6 +4,6 @@ window.addEventListener("keydown", playSound);
 function playSound(e) {
   const audio = document.querySelector(`audio[data-key="${e.key}"]`);
   if (!audio) return;
-  audio.currentTime= 0.02;
+  audio.currentTime= 0;
   audio.play();
 }
